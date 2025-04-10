b4770e831ca0dda49a72c2ceae36edb78f50cf5a
erickleite338 <erickleite338@gmail.com>
2025-04-10 21:23:34 +0000

    feat(projectsData): Add project recommendation list
    - Add `Tailwind Nextjs Starter Blog Alternative`
    - Add `Firebase Studio`

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Tailwind Nextjs Starter Blog Alternative',
+    description: `Meu template de blog rico em recursos, construído com Next.js e Tailwind CSS. Uma alternativa personalizável a blogs estáticos gerados por Jekyll e Hugo.`,
+    imgSrc: '/static/images/tailwind-blog.jpg',
+    href: 'https://tailwind-nextjs-starter-blog-alternative.vercel.app/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Firebase Studio',
+    description: `O Firebase Studio é um espaço de trabalho online com IA generativa (Gemini) integrado, que visa acelerar o desenvolvimento full-stack de aplicativos web e móveis, permitindo criar backends, frontends e implantá-los no Firebase, tudo em um só lugar.`,
+    imgSrc: '/static/images/firebase-studio.png',
+    href: 'https://firebase.studio/',
   },
 ]
 
